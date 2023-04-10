8f6583f2bf8449c7e09e4c8d3c0d4848dea323c3
Sergio Hernández Navarro <sergio.h.navarro@finetwork.com>
2023-04-10 17:25:17 +0200

    create next architecture and mui themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
+import { darkTheme, lightTheme } from '@/themes'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
